091cc0ab2d6c9a49131cd574c78e944c0fbeabf7
GilAddaCyberArk <gil.adda@cyberark.com>
2024-11-12 15:42:24 +0200

    install

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-
+set -x
+set -v
 # Check if git is installed
 if ! command -v git &> /dev/null
 then
@@ -9,9 +10,19 @@ fi
 
 REPO_NAME=https://github.com/GilAddaCyberark/crypto-scanner/
 BRANCH=draft
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo - script dir $SCRIPT_DIR
+TMP_DIR_NAME=tmp
+echo script dir $SCRIPT_DIR
+TMP_DIR_PATH=$SCRIPT_DIR/$TMP_DIR_NAME
+echo temp dir: $TMP_DIR_PATH
+
+mkdir -p $TMP_DIR_NAME
+cd $TMP_DIR_PATH
+echo temp - $(pwd)
+
 # Git clone it
 git clone --branch $BRANCH $REPO_NAME
-
 
 # Check if golang compiler  is installed
 if ! command -v go &> /dev/null
@@ -20,11 +31,19 @@ then
     exit 1
 fi
 
-# Change to the repository directory
-cd crypto-scanner
+cd $TMP_DIR_PATH/crypto-scanner
+echo $(pwd)
+go mod download
 
-# Build the binary-checker tool
-cd cmd/binary-checker
-go build -o binarychecker
+cd $TMP_DIR_PATH/crypto-scanner/cmd/crypto-checker
+echo build dir: $(pwd)
 
-echo "binarychecker tool has been built successfully."
+go build -o ./binary-checker $TMP_DIR_PATH/crypto-scanner/cmd/binary-checker
+
+ls -l $TMP_DIR_PATH/crypto-scanner/cmd/binary-checker
+
+echo "binary-checker tool has been built successfully"
+
+cp $TMP_DIR_PATH/crypto-scanner/binary-checker ../../
+
+rm -rf $TMP_DIR_PATH
